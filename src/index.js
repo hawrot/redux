005ae4f09fcb3784c0228754a16f44718da93b50
hawrot <mhawro200@caledonian.ac.uk>
2020-05-18 23:42:32 +0100

    Connect react with redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore} from 'redux';
+import {createStore } from 'redux';
+import {Provider} from 'react-redux'
 import reducer from './store/reducer';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App />, document.getElementById('root')</Provider> );
 registerServiceWorker();
