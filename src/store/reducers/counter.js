8f5b1c2a2e754e04e4ac926c8a8978da624d607c
hawrot <mhawro200@caledonian.ac.uk>
2020-05-20 22:33:49 +0100

    Split the logic between reducers

---
+++ src/store/reducers/counter.js
@@ -0,0 +1,36 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+    switch ( action.type ) {
+        case actionTypes.INCREMENT:
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+            break;
+        case actionTypes.DECREMENT:
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+            break;
+        case actionTypes.ADD:
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+            break;
+        case actionTypes.SUBTRACT:
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
+    }
+    return state;
+};
+
+export default reducer;
---
+++ src/store/reducers/result.js
@@ -0,0 +1,30 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    results: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch ( action.type ) {
+        case actionTypes.STORE:
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+            }
+            break;
+        case actionTypes.DELETE_RESULT:
+            /* const id = 2;
+             const newArr = [...state.results];
+             newArr.splice(id,1);*/
+
+            const updatedArray = state.results.filter(result =>  result.id !== action.resultElId);
+            return {
+                ...state,
+                results: updatedArray
+            }
+            break;
+    }
+    return state;
+};
+
+export default reducer;
