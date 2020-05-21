b309b79a6c61a7bc8af504bf2f9e4da086d03748
hawrot <mhawro200@caledonian.ac.uk>
2020-05-21 18:53:54 +0100

    Fix the typo

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -8,16 +8,16 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.INCREMENT:
-            return updateObject(state, {counter: state.counter + 1)
+            return updateObject(state, {counter: state.counter + 1});
 
         case actionTypes.DECREMENT:
-            return updateObject(state, {counter: state.counter - 1})
+            return updateObject(state, {counter: state.counter - 1});
 
         case actionTypes.ADD:
-            return updateObject(state, {counter: state.counter + action.value})
+            return updateObject(state, {counter: state.counter + action.value});
 
         case actionTypes.SUBTRACT:
-            return updateObject(state, {counter: state.counter - action.value})
+            return updateObject(state, {counter: state.counter - action.value});
     }
     return state;
 };
