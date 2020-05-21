051ca4c1132d8a85b3adcc9dd2fb9a39e6b879a4
hawrot <mhawro200@caledonian.ac.uk>
2020-05-21 18:45:21 +0100

    Getting state in the actions

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -1,21 +1,21 @@
 import * as actionTypes from "./actionsTypes";
 
-export const saveResult = (res) =>{
+export const saveResult = (res) => {
+    //const updatedResult = res * 2;
     return {
         type: actionTypes.STORE,
         result: res
     };
 }
 
-
 export const store = (res) => {
-    return dispatch => {
+    return (dispatch, getState) => {
         setTimeout(() => {
+            const oldCounter = getState().ctr.counter;
+            console.log('[Async] Old Counter: ' + oldCounter);
             dispatch(saveResult(res))
         }, 2000)
     }
-
-
 };
 export const deleteResult = (resElId) => {
     return {
